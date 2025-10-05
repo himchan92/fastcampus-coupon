2fe69dfc7af12da560cd220a48bc8e2cbfd4fad4
omea92 <thdjun92@naver.com>
2025-10-05 13:40:56 +0900

    init

---
+++ build.gradle.kts
@@ -0,0 +1,61 @@
+val bootJar: org.springframework.boot.gradle.tasks.bundling.BootJar by tasks
+
+bootJar.enabled = false
+
+plugins {
+    java
+    id("org.springframework.boot") version "3.5.6"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.fastcampus"
+version = "0.0.1-SNAPSHOT"
+description = "coupon"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+subprojects {
+    apply(plugin = "java")
+    apply(plugin = "io.spring.dependency-management")
+    apply(plugin = "org.springframework.boot")
+
+    repositories {
+        mavenCentral()
+    }
+
+    dependencies {
+        implementation("org.springframework.boot:spring-boot-starter")
+        implementation("org.springframework.boot:spring-boot-starter-data-redis")
+        implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+        compileOnly("org.projectlombok:lombok")
+        annotationProcessor("org.projectlombok:lombok")
+
+        testCompileOnly("org.projectlombok:lombok")
+        testAnnotationProcessor("org.projectlombok:lombok")
+
+        runtimeOnly("com.h2database:h2")
+
+        runtimeOnly("com.mysql:mysql-connector-j")
+        testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
