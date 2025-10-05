4c4fa06d16bc4d0a96de6c0e40b5bacc4493bf91
omea92 <thdjun92@naver.com>
2025-10-05 23:51:37 +0900

    QueryDsl 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,6 +53,11 @@ subprojects {
 
         runtimeOnly("com.mysql:mysql-connector-j")
         testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+        implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
+        annotationProcessor("com.querydsl:querydsl-apt:5.0.0:jakarta")
+        annotationProcessor("jakarta.annotation:jakarta.annotation-api")
+        annotationProcessor("jakarta.persistence:jakarta.persistence-api")
     }
 }
 
---
+++ coupon-core/src/main/java/com/fastcampus/couponcore/configuration/QueryDslConfiguration.java
@@ -0,0 +1,22 @@
+package com.fastcampus.couponcore.configuration;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+// TODO: QueryDSL 설정
+@Configuration
+@RequiredArgsConstructor
+public class QueryDslConfiguration {
+
+    @PersistenceContext
+    private final EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(){
+        return new JPAQueryFactory(entityManager);
+    }
+}
