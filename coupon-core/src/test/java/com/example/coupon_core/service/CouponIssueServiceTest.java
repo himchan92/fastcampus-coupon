af8bce169f817a8476ca29f3ac238e89da5e9f9b
omea92 <thdjun92@naver.com>
2025-11-23 01:40:24 +0900

    쿠폰발급 JUnit

--- coupon-core/src/test/java/com/example/coupon_core/service/CouponIssueServiceTest.java
+++ coupon-core/src/test/java/com/example/coupon_core/service/CouponIssueServiceTest.java
@@ -1,5 +1,55 @@
 package com.example.coupon_core.service;
 
-class CouponIssueServiceTest {
+import com.example.coupon_core.TestConfig;
+import com.example.coupon_core.exception.CouponIssueException;
+import com.example.coupon_core.exception.ErrorCode;
+import com.example.coupon_core.model.CouponIssue;
+import com.example.coupon_core.repository.mysql.CouponIssueJpaRepository;
+import com.example.coupon_core.repository.mysql.CouponIssueRepository;
+import com.example.coupon_core.repository.mysql.CouponJpaRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class CouponIssueServiceTest extends TestConfig {
+
+    @Autowired
+    CouponIssueService sut;
+
+    @Autowired
+    CouponIssueJpaRepository couponIssueJpaRepository;
+
+    @Autowired
+    CouponIssueRepository couponIssueRepository;
+
+    @Autowired
+    CouponJpaRepository couponJpaRepository;
+
+    @BeforeEach
+    void clean() {
+        couponJpaRepository.deleteAllInBatch();
+        couponIssueJpaRepository.deleteAllInBatch();
+    }
+
+    @Test
+    @DisplayName("쿠폰 발급 내역이 존재하면 예외 발생")
+    void saveCouponIssue_1() {
+        CouponIssue couponIssue = CouponIssue.builder()
+                .couponId(1L)
+                .userId(1L)
+                .build();
+
+        couponIssueJpaRepository.save(couponIssue);
+
+        CouponIssueException exception = assertThrows(CouponIssueException.class, () -> {
+            sut.saveCouponIssue(couponIssue.getCouponId(), couponIssue.getUserId());
+        });
+
+        assertEquals(exception.getErrorCode(), ErrorCode.DUPLICATED_COUPON_ISSUE);
+    }
 }
