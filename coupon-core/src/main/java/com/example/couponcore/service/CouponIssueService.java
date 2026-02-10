d96836c77111f07a0daf693f93f9daaf2a6462c1
omea92 <thdjun92@naver.com>
2026-02-10 23:06:48 +0900

    쿠폰 발급 서비스

---
+++ coupon-core/src/main/java/com/example/couponcore/service/CouponIssueService.java
@@ -0,0 +1,57 @@
+package com.example.couponcore.service;
+
+import com.example.couponcore.exception.CouponIssueException;
+import com.example.couponcore.model.Coupon;
+import com.example.couponcore.model.CouponIssue;
+import com.example.couponcore.repository.mysql.CouponIssueJpaRepository;
+import com.example.couponcore.repository.mysql.CouponIssueRepository;
+import com.example.couponcore.repository.mysql.CouponJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import static com.example.couponcore.exception.ErrorCode.COUPON_NOT_EXIST;
+import static com.example.couponcore.exception.ErrorCode.DUPLICATED_COUPON_ISSUE;
+
+@Service
+@RequiredArgsConstructor
+public class CouponIssueService {
+
+    private final CouponJpaRepository couponJpaRepository;
+    private final CouponIssueJpaRepository couponIssueJpaRepository;
+    private final CouponIssueRepository couponIssueRepository;
+
+    @Transactional
+    public void issue(long couponId, long userId) {
+        Coupon coupon = findCoupon(couponId); //쿠폰정보 가져오기
+        coupon.issue(); //발급, 기간 유효성 검증
+        saveCouponIssue(couponId, userId); //어떤 사용자가 어떤 쿠폰발급받았는지 체크
+    }
+
+    @Transactional(readOnly = true)
+    public Coupon findCoupon(long couponId) {
+        return couponJpaRepository.findById(couponId).orElseThrow(() -> {
+            throw new CouponIssueException(COUPON_NOT_EXIST, "쿠폰 정책이 존재하지 않습니다. %s".formatted(couponId));
+        });
+    }
+
+    @Transactional
+    public CouponIssue saveCouponIssue(long couponId, long userId) {
+        checkAlreadyIssuance(couponId, userId); //이미 쿠폰발급되었는지 체크
+
+        CouponIssue issue = CouponIssue.builder()
+                .couponId(couponId)
+                .userId(userId)
+                .build();
+        return couponIssueJpaRepository.save(issue);
+    }
+
+    private void checkAlreadyIssuance(long couponId, long userId) {
+        CouponIssue issue = couponIssueRepository.findFirstCouponIssue(couponId, userId);
+
+        //이미 발급된 상태면 발급되면 안된다.
+        if(issue != null) {
+            throw new CouponIssueException(DUPLICATED_COUPON_ISSUE, "이미 발급된 쿠폰입니다. user_id: %s, couponId: %s".formatted(userId, couponId));
+        }
+    }
+}
---
+++ coupon-core/src/test/java/com/example/couponcore/service/CouponIssueServiceTest.java
@@ -0,0 +1,55 @@
+package com.example.couponcore.service;
+
+import com.example.couponcore.TestConfig;
+import com.example.couponcore.exception.CouponIssueException;
+import com.example.couponcore.exception.ErrorCode;
+import com.example.couponcore.model.CouponIssue;
+import com.example.couponcore.repository.mysql.CouponIssueJpaRepository;
+import com.example.couponcore.repository.mysql.CouponIssueRepository;
+import com.example.couponcore.repository.mysql.CouponJpaRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class CouponIssueServiceTest extends TestConfig {
+
+    @Autowired
+    CouponIssueService sut;
+
+    @Autowired
+    CouponIssueJpaRepository couponIssueJpaRepository;
+
+    @Autowired
+    CouponIssueRepository couponIssueRepository;
+
+    @Autowired
+    CouponJpaRepository couponJpaRepository;
+
+    @BeforeEach
+    void clean() {
+        couponJpaRepository.deleteAllInBatch();
+        couponIssueJpaRepository.deleteAllInBatch();
+    }
+
+    @Test
+    @DisplayName("쿠폰 발급 내역이 존재하면 예외를 반환한다")
+    void saveCouponIssue_1() {
+        //미리 저장해서 발급 상태 만듬
+        CouponIssue couponIssue = CouponIssue.builder()
+                .couponId(1L)
+                .userId(1L)
+                .build();
+        couponIssueJpaRepository.save(couponIssue);
+
+        //예외 잘던지는지 체크
+        CouponIssueException exception = assertThrows(CouponIssueException.class, () -> {
+            sut.saveCouponIssue(couponIssue.getCouponId(), couponIssue.getUserId());
+        });
+
+        assertEquals(exception.getErrorCode(), ErrorCode.DUPLICATED_COUPON_ISSUE);
+    }
+}
